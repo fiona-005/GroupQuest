7905c3ce2ff18369104f53e41144cc633ce837b0
teresax14 <st0404@mci4me.at>
2026-05-06 14:54:24 +0200

    gelöscht

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
 import streamlit as st
 
-st.title("GroupQuest")
-st.markdown("Hello World!")
-st.markdown("Hello World!")
-st.markdown("HELLOOOO")
-st.markdown("Hello friends!")
-st.markdown("i hab keine geschwungene Klammer")
-st.markdown("ERROR")
-st.text_input("Motivationssprüche 🍓")
