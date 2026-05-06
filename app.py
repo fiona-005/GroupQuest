c9fef9c380a3535718cd6868b4208edfeabb0524
fiona-005 <fiona.neurauter@gmail.com>
2026-05-06 15:12:50 +0200

    Navigation Update

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import streamlit as st
 st.title("🍓Questify🍓")
 
 def page1():
-    st.write(st.session_state.foo)
+    st.markdown("Account erstellen")
 
 def page2():
-    st.write(st.session_state.bar)
+    st.markdown("Quest erstellen")
 
 # Widgets shared by all the pages
-st.sidebar.selectbox("Foo", ["A", "B", "C"], key="foo")
-st.sidebar.checkbox("Bar", key="bar")
+if st.sidebar.button("Account erstellen"):
+    page1()
+if st.sidebar.button("Quest erstellen"):
+    page2()
