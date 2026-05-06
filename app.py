62c78a55a4c3e48268984a00f8fa22a362c590f3
teresax14 <st0404@mci4me.at>
2026-05-06 14:36:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,7 @@ import streamlit as st
 st.title("GroupQuest")
 st.markdown("Hello World!")
 st.markdown("Hello World!")
-<<<<<<< HEAD
-
-st.markdown("Hello World!")
-st.markdown("Hello World!")
-
-st.markdown("Hello friends!")
-=======
 st.markdown("HELLOOOO")
 st.markdown("Hello friends!")
 st.markdown("i hab keine geschwungene Klammer")
 st.markdown("ERROR")
->>>>>>> 7524ad7c5096bd9db37563be2667624af2938ac5
