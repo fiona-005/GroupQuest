694fa6c874dc8a8a1f4a2a44121e6e83caea3a3d
fiona-005 <fiona.neurauter@gmail.com>
2026-05-06 15:04:20 +0200

    navigation

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title("🍓Questify🍓")
 
+def page1():
+    st.write(st.session_state.foo)
+
+def page2():
+    st.write(st.session_state.bar)
+
+# Widgets shared by all the pages
+st.sidebar.selectbox("Foo", ["A", "B", "C"], key="foo")
+st.sidebar.checkbox("Bar", key="bar")
